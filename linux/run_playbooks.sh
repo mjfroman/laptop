e2bed03418d8371275c6d041775444f730cc048c
Michael Froman <mjfroman@mac.com>
2018-05-30 20:00:16 -0700

    Fix the hosts path on ansible commands.

--- linux/run_playbooks.sh
+++ linux/run_playbooks.sh
@@ -2,9 +2,9 @@
 
 echo "Running playbooks..."
 
-ansible-playbook linux-playbook.yml -i hosts --ask-sudo-pass -vvvv 
+ansible-playbook linux-playbook.yml -i ../hosts --ask-sudo-pass -vvvv 
 source ~/.profile
-ansible-playbook clone-firefox-playbook.yml -i hosts -vvvv 
+ansible-playbook clone-firefox-playbook.yml -i ../hosts -vvvv 
 
 echo "Done running playbooks"
 
