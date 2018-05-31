fb11bf515b1af168ba5036d6866e7239c0a3b437
Michael Froman <mjfroman@mac.com>
2018-05-30 17:00:56 -0700

    Add the ansible commands to the linux script.

--- linux/run_playbooks.sh
+++ linux/run_playbooks.sh
@@ -2,5 +2,9 @@
 
 echo "Running playbooks..."
 
+ansible-playbook linux-playbook.yml -i hosts --ask-sudo-pass -vvvv 
+source ~/.profile
+ansible-playbook clone-firefox-playbook.yml -i hosts -vvvv 
+
 echo "Done running playbooks"
 
