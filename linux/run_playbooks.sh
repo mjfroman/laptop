053fcfda57e1a052f46ee5b8d6d0488e8eaf55f8
Michael Froman <mjfroman@mac.com>
2018-06-07 06:32:30 -0700

    Add rust playbook to run script.

--- linux/run_playbooks.sh
+++ linux/run_playbooks.sh
@@ -3,6 +3,7 @@
 echo "Running playbooks..."
 
 ansible-playbook linux-playbook.yml -i ../hosts --ask-sudo-pass -vvvv
+ansible-playbook rust-playbook.yml -i ../hosts -vvvv
 source ~/.profile
 ansible-playbook clone-firefox-playbook.yml -i ../hosts -vvvv
 
