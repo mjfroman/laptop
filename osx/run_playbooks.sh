6ed31b942a8be113e97a70f2c52b56f6904bd111
Michael Froman <mjfroman@mac.com>
2018-12-13 11:57:07 -0600

    Make sure we install ccache for builds.

--- osx/run_playbooks.sh
+++ osx/run_playbooks.sh
@@ -2,6 +2,12 @@
 
 echo "Running playbooks..."
 
+# for now we're creating /MozillaBuilds here w/ sudo
+echo "Asking for sudo password to create /MozillaBuilds"
+sudo mkdir -p /MozillaBuilds
+sudo chown `whoami` /MozillaBuilds
+
+ansible-playbook minimal_playbook.yml -i ../hosts -vvvv
 ansible-playbook ../linux/rust-playbook.yml -i ../hosts -vvvv
 source ~/.profile
 ansible-playbook clone-firefox-playbook.yml -i ../hosts -vvvv
