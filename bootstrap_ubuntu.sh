d42b886c603be709e3bb14dd9970b5c32a65d2ec
Michael Froman <mjfroman@mac.com>
2019-04-19 15:18:44 -0500

    Change mercurial-setup to vcs-setup and fix some paths for ease of use.

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -73,8 +73,8 @@ cd laptop
 
 echo "run: scp fromanavc.net:get_env.sh . && sh get_env.sh"
 echo "logout and login to ensure profile changes are picked up."
-echo "run: source .profile && cd ~/mozilla/moz-central \\"
-echo "     && ./mach mercurial-setup --update \\"
+echo "run: source ~/.profile && cd ~/mozilla/moz-central \\"
+echo "     && ./mach vcs-setup --update \\"
 echo "     && ./mach bootstrap \\"
 echo "     && ./mach eslint --setup"
 
