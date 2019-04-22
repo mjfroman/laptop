491a533d6b9bbcef303de9cc9a0d7fbc2101ee62
Michael Froman <mjfroman@mac.com>
2019-04-22 17:26:06 -0500

    Add switchconfig line to output for final steps.

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -80,5 +80,6 @@ echo "logout and login to ensure profile changes are picked up."
 echo "run: source ~/.profile && cd ~/mozilla/moz-central \\"
 echo "     && ./mach vcs-setup --update \\"
 echo "     && ./mach bootstrap \\"
-echo "     && ./mach eslint --setup"
+echo "     && ./mach eslint --setup \\"
+echo "     && switchconfig.sh deb"
 
