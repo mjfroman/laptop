8a625a344bea75ef88177516c4332f224e5a72da
Michael Froman <mjfroman@mac.com>
2022-01-17 15:08:09 -0600

    Make sure to source ~/.profile so we pick up brew env earlier.

--- osx/run_playbooks.sh
+++ osx/run_playbooks.sh
@@ -5,6 +5,8 @@ echo "Running playbooks..."
 # for now we're creating /MozillaBuilds here w/ sudo
 mkdir -p no_tm_backup/MozillaBuilds
 
+source ~/.profile
+
 ansible-playbook minimal_playbook.yml -i ../hosts -vvvv
 ansible-playbook ../linux/rust-playbook.yml -i ../hosts -vvvv
 source ~/.profile
