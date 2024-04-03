28b48a779e7304b533150b96245e4d9fa873fec6
Michael Froman <mjfroman@mac.com>
2024-04-03 18:15:13 -0500

    When printing final manual steps, make sure to save/restore the .hgrc file

--- scripts/show_final_message.sh
+++ scripts/show_final_message.sh
@@ -3,7 +3,9 @@
 echo "run: scp fromanavc.net:get_env.sh . && sh get_env.sh"
 echo "logout and login to ensure profile changes are picked up."
 echo "run: source ~/.profile && cd ~/mozilla/moz-central \\"
-echo "     && ./mach vcs-setup --update \\"
-echo "     && ./mach bootstrap --application-choice=browser \\"
+echo "     && cp ~/.hgrc ~/hgrc-bak \\" # save the current .hgrc file
+echo "     && ./mach vcs-setup \\" # answer yes to everything to install evolve, etc
+echo "     && cat ~/hgrc-bak > ~/.hgrc && rm ~/hgrc-bak \\" # restore the .hgrc file
+echo "     && ./mach --no-interactive bootstrap --application-choice=browser \\"
 echo "     && ./mach eslint --setup \\"
 echo "     && switchconfig.sh deb"
